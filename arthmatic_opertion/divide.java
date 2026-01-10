2af01331ef42fdcb85c9d4d52f752a6c2973c4cd
avhadharshad268 <avhadharshad268@gmail.com>
2026-01-10 23:39:14 +0530

    Harshad

--- arthmatic_opertion/divide.java
+++ arthmatic_opertion/divide.java
@@ -3,7 +3,7 @@ package arthmatic_opertion;
 public class divide {
 
 	public static void main (String []args ) {
-		long   num5 = 90 ;
+		long   num5 = 80 ;
 		long  num6 = 89 ;
 		long divide = num5/num6;
 		System.out.println("divide of two number = " +  divide);
