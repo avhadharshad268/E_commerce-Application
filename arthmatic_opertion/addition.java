790c0b60ac79bd68bee63348c0c000b5e559b0cf
avhadharshad268 <avhadharshad268@gmail.com>
2026-01-11 09:32:38 +0530

    commit

--- arthmatic_opertion/addition.java
+++ arthmatic_opertion/addition.java
@@ -4,7 +4,7 @@ public class addition {
 	
 	public static void main (String  [] args ) {
 		int num1 = 7890;
-		int num2 = 8990;
+		int num2 = 89978;
 		
 		int  addition = num1 + num2 ;
 		System.out.println(" addition of two number = " + addition);
