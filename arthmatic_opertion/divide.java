b6e6529eabc6527c7cfe592fd84bfc02b6a11bc8
avhadharshad268 <avhadharshad268@gmail.com>
2026-01-11 07:07:35 +0530

    Number Changes

--- arthmatic_opertion/divide.java
+++ arthmatic_opertion/divide.java
@@ -4,7 +4,7 @@ public class divide {
 
 	public static void main (String []args ) {
 		long   num5 = 80 ;
-		long  num6 = 89 ;
+		long  num6 = 80 ;
 		long divide = num5/num6;
 		System.out.println("divide of two number = " +  divide);
 	}
