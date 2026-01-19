62b3fa044140ceb12a7b8cd6f0c735387be80950
avhadharshad268 <avhadharshad268@gmail.com>
2026-01-19 13:16:24 +0530

    Make option of Product And User  management

--- src/main/java/main/E_commerce.java
+++ src/main/java/main/E_commerce.java
@@ -12,6 +12,29 @@ public class E_commerce {
 		System.out.println("Entre Login ID  and password  ");
 		
         login.login();
+        Scanner sc = new Scanner(System.in);
+        int option ;
+        
+        while (true) {
+    		System.out.println("*MENU*");
+    		System.out.println("1. Product Mangement");
+    		System.out.println("2. User  Mangement \"");
+    		System.out.println("3. Exit From App \"");
+    		System.out.println("Entre your option");
+
+
+        	option = sc.nextInt();
+        	if (option == 1) {
+        		System.out.println("1. Product Mangement ");
+        	} else if (option == 2) {
+        		System.out.println("2. User  Mangement ");
+        	} else if (option == 3) {
+        		System.out.println("3. Exit From App ");
+        		System.exit(0);
+        	} else {
+        		System.out.println("Hey User Choose Wrong Option");
+        	}
+        }
 }
 	   
 	      
